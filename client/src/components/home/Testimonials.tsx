d7138252f48713c600e7da6e1aa9c551244f43e7
Tanmoy70 <tanmoykanrar1@gmail.com>
2026-03-20 01:13:42 +0530

    Added code for more clarify ofr future understanding

--- client/src/components/home/Testimonials.tsx
+++ client/src/components/home/Testimonials.tsx
@@ -4,7 +4,7 @@ import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-
+// Mail testimonial section
 const testimonials = [
   {
     name: 'Aarav Sharma',
