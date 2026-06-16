de416a972aff61e673c9e388feef71e1aafcb2f9
Tanmoy70 <tanmoykanrar1@gmail.com>
2026-06-16 23:59:23 +0530

    Add comment to clarify the purpose of the Contact component

--- client/src/components/home/Contact.tsx
+++ client/src/components/home/Contact.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// COmtact functional component for the contact section of the homepage
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: '',
